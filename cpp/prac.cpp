9a01d50750355c15f31d385abda5b304ca222b65
shreyamandal123 <56383115+shreyamandal123@users.noreply.github.com>
2019-10-19 16:49:30 +0530

    To interchange numbers in c++

---
+++ cpp/prac.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+void main()
+{
+        int a,b,c;
+	cout<<"enter number a=";
+	cin>>a;
+	cout<<"\n enter number b=";
+	cin>>b;
+	a=c;
+	a=b;
+	b=c;
+	cout<<"\n now a ="<<a;
+	cout<<"\n now b="<<b;
+	
+}
